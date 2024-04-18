774edd3efc7b09c3dbfeba30688d29076edd57f3
osamamypc <osamamypc@gmail.com>
2024-04-18 10:40:20 +0800

    new

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
